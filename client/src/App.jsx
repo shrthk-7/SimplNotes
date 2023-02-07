8dfe71d133c333d660d628a78bba0569feba3c69
Sharthak Sharma <sharthaksharma.ss@gmail.com>
2023-02-07 22:09:05 +0530

    merged ui-improv on users

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,34 +3,43 @@ import NoteCard from "./components/NoteCard";
 import AddNoteButton from "./components/AddNoteButton";
 import AddNote from "./components/AddNote";
 import "./App.css";
+import Signup from "./components/Signup";
+
+// const App = () => {
+//   const [backendData, setBackendData] = useState([]);
+//   const [addNote, setAddNote] = useState(false);
+
+//   useEffect(() => {
+//     async function fetchData() {
+//       const res = await fetch("/api");
+//       const data = await res.json();
+//       setBackendData(data.notes);
+//     }
+//     fetchData();
+//   }, []);
+
+//   return (
+//     <div className="main" key={backendData}>
+//       <h1 className="main-heading">NOTES</h1>
+//       <div className="notes-container">
+//         {backendData.map((note) => {
+//           return <NoteCard key={note.id} note={note} />;
+//         })}
+//       </div>
+//       {addNote ? (
+//         <AddNote setAddNote={setAddNote} setBackendData={setBackendData} />
+//       ) : (
+//         <AddNoteButton setAddNote={setAddNote} />
+//       )}
+//     </div>
+//   );
+// };
 
 const App = () => {
-  const [backendData, setBackendData] = useState([]);
-  const [addNote, setAddNote] = useState(false);
-
-  useEffect(() => {
-    async function fetchData() {
-      const res = await fetch("/api");
-      const data = await res.json();
-      setBackendData(data.notes);
-    }
-    fetchData();
-  }, []);
-
   return (
-    <div className="main" key={backendData}>
-      <h1 className="main-heading">NOTES</h1>
-      <div className="notes-container">
-        {backendData.map((note) => {
-          return <NoteCard key={note.id} note={note} />;
-        })}
-      </div>
-      {addNote ? (
-        <AddNote setAddNote={setAddNote} setBackendData={setBackendData} />
-      ) : (
-        <AddNoteButton setAddNote={setAddNote} />
-      )}
-    </div>
+    <>
+      <Signup />
+    </>
   );
 };
 
--- server/router/notesRouter.js
+++ server/router/notesRouter.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const router = express.Router();
 const notesController = require("../controller/notesController");
 
+const router = express.Router();
 router
   .route("/")
   .get(notesController.getAllNotes)
